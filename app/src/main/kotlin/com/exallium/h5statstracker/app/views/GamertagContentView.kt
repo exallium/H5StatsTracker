65056e2232a8598a8ee6e5a78d218dfda9cc20c0
Alex Hart <alex.hart@redspace.com>
2015-11-13 17:01:13 -0400

    Add Gamertag input error and keyboard hiding

--- app/src/main/kotlin/com/exallium/h5statstracker/app/views/GamertagContentView.kt
+++ app/src/main/kotlin/com/exallium/h5statstracker/app/views/GamertagContentView.kt
@@ -2,7 +2,10 @@ package com.exallium.h5statstracker.app.views
 
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.LinearLayout
 import com.exallium.h5statstracker.app.Constants
@@ -20,7 +23,15 @@ public class GamertagContentView(context: Context?,
     private fun applyState(bundle: Bundle?) {
         (findViewById(R.id.input_gamertag) as EditText).setText(bundle?.getString(Constants.GAMERTAG, ""))
         (findViewById(R.id.input_submit)).setOnClickListener {
-            controller.onSubmitGamertag((findViewById(R.id.input_gamertag) as EditText).text.toString())
+            val gamertagOk = controller.onSubmitGamertag((findViewById(R.id.input_gamertag) as EditText).text.toString())
+            if (gamertagOk) {
+                val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager;
+                imm.hideSoftInputFromWindow(this.windowToken, 0);
+            } else {
+                val gamertagInputLayout = findViewById(R.id.input_layout_gamertag) as TextInputLayout
+                gamertagInputLayout.error = context.getString(R.string.gamertag_input_error)
+                gamertagInputLayout.isErrorEnabled = true
+            }
         }
     }
 }
