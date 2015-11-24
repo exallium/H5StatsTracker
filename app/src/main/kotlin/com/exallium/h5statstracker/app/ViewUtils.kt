aeac5bd70a35c4739127b1d45fadadfe0fe2d920
Alex Hart <alex.hart@redspace.com>
2015-11-24 10:55:38 -0400

    Start adding base work for Medals

--- app/src/main/kotlin/com/exallium/h5statstracker/app/ViewUtils.kt
+++ app/src/main/kotlin/com/exallium/h5statstracker/app/ViewUtils.kt
@@ -1,11 +1,17 @@
 package com.exallium.h5statstracker.app
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import android.widget.ImageView
+import com.exallium.h5.api.models.metadata.Medal
 import com.exallium.h5statstracker.app.views.GamertagContentView
 import com.exallium.h5statstracker.app.views.infographic.InfographicAdapter
 import com.exallium.h5statstracker.app.views.infographic.impl.servicereports.arena.ArenaDataFactory
@@ -14,6 +20,11 @@ import com.exallium.h5statstracker.app.views.infographic.impl.servicereports.com
 import com.exallium.h5statstracker.app.views.infographic.impl.servicereports.custom.CustomDataFactory
 import com.exallium.h5statstracker.app.views.infographic.impl.servicereports.multiplayer.MultiplayerDataFactory
 import com.exallium.h5statstracker.app.views.infographic.impl.servicereports.warzone.WarzoneDataFactory
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
+import com.squareup.picasso.Transformation
+import timber.log.Timber
+import java.util.*
 
 
 public fun getRouterView(request: Router.Request, context: Context, controller: MainController): View {
@@ -44,4 +55,60 @@ public fun getRouterView(request: Router.Request, context: Context, controller: 
         else -> throw IllegalStateException("Unknown Route")
     }
     return view
+}
+
+class MedalSpriteTarget(val medal: Medal, val view: ImageView) : Target {
+
+    companion object {
+        val MEDAL_CACHE = HashMap<Medal, Bitmap?>()
+    }
+
+    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+        Timber.v("Preparing to load Medal Sprite")
+    }
+
+    override fun onBitmapFailed(errorDrawable: Drawable?) {
+        Timber.v("Failed to load Medal Sprite")
+    }
+
+    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+        val cachedMedal = MEDAL_CACHE[medal]
+        if (cachedMedal == null) {
+            val medalImage = MedalSpriteSheetTransformation(medal).transform(bitmap)
+            view.setImageBitmap(medalImage)
+            MEDAL_CACHE[medal] = medalImage
+        } else {
+            view.setImageBitmap(cachedMedal)
+        }
+    }
+
+}
+
+class MedalSpriteSheetTransformation(medal: Medal) : Transformation {
+
+    private val x: Int
+    private val y: Int
+    private val w: Int
+    private val h: Int
+    private val medalName: String
+
+    init {
+        x = medal.spriteLocation.left
+        y = medal.spriteLocation.top
+        w = medal.spriteLocation.width
+        h = medal.spriteLocation.height
+        medalName = medal.name
+    }
+
+    override fun key(): String? = "tf:medal:%s:%d:%d:%d:%d".format(medalName, x, y, w, h)
+
+    override fun transform(source: Bitmap?): Bitmap? {
+        val b = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        val c = Canvas(b)
+        val src = Rect(x, y, x + w, y + h)
+        val dst = Rect(0, 0, w, h)
+        c.drawBitmap(source, src, dst, null)
+        return b
+    }
+
 }
---
+++ app/src/main/kotlin/com/exallium/h5statstracker/app/views/infographic/impl/medals/viewmodels.kt
@@ -0,0 +1,5 @@
+package com.exallium.h5statstracker.app.views.infographic.impl.medals
+
+/**
+ * Created by ahart on 2015-11-24.
+ */
